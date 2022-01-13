6fe713e45b8c2195ee566bec12ad08d6d308839d
Kottra Richárd <kottra.richard@jedlik.eu>
2022-01-13 18:02:55 +0100

    pilóták lekérdezése

--- app/Http/Controllers/Pilota.php
+++ app/Http/Controllers/Pilota.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 
 class Pilota extends Controller
 {
-    //
+    public function getPilotak(Request $request) {
+        
+        $query = $request->input('q');
+        
+        if ($query == "") {
+            $pilotak = DB::select("SELECT * FROM versenyzok");
+        } else {
+            $pilotak = DB::select("SELECT * FROM versenyzok WHERE nemzet = ?", array($query));
+        }
+        return $pilotak;
+    }
 }
